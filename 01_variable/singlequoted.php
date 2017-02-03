503d16ae75ce06227fdd2bd914b9a03d1f9e1272
fidiatus sakinah <fidiatussakinah@gmail.com>
2017-02-03 15:28:37 +0700

    update boolean

---
+++ 01_variable/singlequoted.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Single quode</title>
+</head>
+<?php
+$string1='String sederhana';
+$string2='string banyak baris';
+$string3='kalimat tidak pindah';
+$string4='string1 dan string3 tidak tampil';
+echo $string1; echo "<br>";
+echo $string2; echo "<br>";
+echo $string3; echo "<br>";
+echo $string4; 
+?>
+<body>
+</body>
+</html>
