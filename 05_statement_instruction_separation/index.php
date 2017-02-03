0002fb3e82a61c026bea1a124a2d91f4f9dfc9c4
rikoseptaA <apmponorogo@gmail.com>
2017-02-03 15:52:36 +0700

    pertama

---
+++ 05_statement_instruction_separation/index.php
@@ -0,0 +1,16 @@
+<?php
+$Makanan = 5;
+/**Basic Syntax: Statement
+*PHP statement adalah pernyataan kondisi dimana
+// if ($Camilan == 1) {
+//     echo "Ini Camilan Roti";
+// } else {
+//     echo "Bukan Camilan Roti";
+// }
+**/
+$Makanan = [
+    1 => 'ayam goreng',
+   	2 => 'nasi goreng',
+    3 => 'tahu goreng',
+];
+var_dump($Makanan);
