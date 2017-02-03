792edfd8f5bbf16052ebde68a6796631de823faf
agnes44 <agnescarolina44@gmail.com>
2017-02-03 14:58:59 +0700

    pertama

---
+++ 03_echo_and_print/integer_literal/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* integer literal dibedakan dalam beberapa format yaitu desimal, oktal, dan hexadesimal
+
+* default tipe datanya int
+
+*/
+
+$ex = 28;
+var_dump($ex);
+
+//hasil int(28)
---
+++ 03_echo_and_print/string_literal/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+* syntak untuk string literal menggunakan double quotes guna mewakili beberapa karakter
+
+*/
+
+$pesan ="Semoga anda berhasil";
+echo "$pesan";    // hasil Semoga anda berhasil
+echo "<br>";
+var_dump($pesan);    // hasil string(20)"Semoga anda berhasil"
