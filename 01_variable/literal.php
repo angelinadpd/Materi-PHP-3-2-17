684bd880505d1b4457fa37e8015647c330ca10d1
fidiatus sakinah <fidiatussakinah@gmail.com>
2017-02-03 15:13:01 +0700

    update boolean

---
+++ 01_variable/literal.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Literal String</title>
+</head>
+<?php
+.
+echo "judul Buku : " .$buku."<br>";
+echo "Nama penulis: ".$penulis."<br>";
+echo "Tahun Terbit : " . $tahun."<br>";
+?>
+<body>
+</body>
+</html>
