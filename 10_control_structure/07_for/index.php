a21a424d92aa7b9a6a31101ba0bb821dea357b3b
MochSukronRizky <kikiponorog0@yahoo.co.id>
2017-02-04 07:48:20 +0700

    control structure

--- 10_control_structure/07_for/index.php
+++ 10_control_structure/07_for/index.php
@@ -1,4 +1,22 @@
 <?php
 
+/**
+* Perulangan for
+* Syntax :
+
+    for(exp,kondisi,exp2){
+        #code yang akan di ulang
+    }
+
+* jika kondisi true, perulangan akan di jalankan
+* jika kondisi false, perulangan akan berhenti
+
+* exp2 akan dijalankan setelah mengulangi code di dalam for
+*/
+
+for($i=10;$i<50;i++)
+{
+    echo $i;
+}
 
 ?>
